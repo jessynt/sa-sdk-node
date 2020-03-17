2c200e887437437aa2843e6a8e4b7fc1a49a8a13
Jessynt <xr5299@gmail.com>
2020-03-17 16:34:51 +0800

    chore: remove non-es6 support
    Signed-off-by: Jessynt <xr5299@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import Submitter from './Submitter'
 import SensorsAnalytics from './SensorsAnalytics'
 
-// Use module.exports to ensure compatiblity with non-es6 projects
-exports = module.exports = SensorsAnalytics
-exports.Submitter = Submitter
+export {Submitter}
+export default SensorsAnalytics
